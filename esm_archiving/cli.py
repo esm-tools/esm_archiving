6c17b5a79cc8032930883a86648e00a37dc0d454
Paul Gierz <pgierz@awi.de>
2020-03-02 17:41:23 +0100

    starts the cli

--- esm_archiving/cli.py
+++ esm_archiving/cli.py
@@ -2,10 +2,22 @@
 
 """Console script for esm_archiving."""
 import sys
+import os
 import click
+import pprint
 
 
-@click.command()
+
+from .esm_archiving import *
+
+
+
+
+pp = pprint.PrettyPrinter(width=41, compact=True)
+
+
+@click.group()
+@click.version_option()
 def main(args=None):
     """Console script for esm_archiving."""
     click.echo("Replace this message by putting your code into "
@@ -13,6 +25,30 @@ def main(args=None):
     click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
+@main.command()
+@click.argument("base_dir")
+@click.argument("filetype")
+@click.argument("start_date")
+@click.argument("end_date")
+def create_archive(base_dir, filetype, start_date, end_date):
+    click.secho("Creating archives for:", color="green")
+    click.secho(base_dir, color="green")
+    click.secho("File: %s" % filetype, color="green")
+    click.secho("From: %s" % start_date, color="green")
+    click.secho("To: %s" % end_date, color="green")
+    files = group_files(base_dir, filetype)
+    files = stamp_files(files)
+
+    config = {
+            "echam": {"archive": {"frequency": "1M", "date_format": "%Y%m"}},
+        "jsbach": {"archive": {"frequency": "1M", "date_format": "%Y%m"}},
+        "hdmodel": {"archive": {"frequency": "1M", "date_format": "%Y%m"}},
+        "fesom": {"archive": {"frequency": "1Y", "date_format": "%Y%m"}},
+        "oasis3mct": {"archive": {"frequency": "1M", "date_format": "%Y%m"}},
+    }
+
+    files = sort_files_to_tarlists(files, start_date, end_date, config)
+    pp.pprint(files)
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
