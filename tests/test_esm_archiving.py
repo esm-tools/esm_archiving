40e40aa57ba50f5ba1ca63b1882a0d250dd6872d
Paul Gierz <pgierz@awi.de>
2020-03-13 13:34:55 +0100

    dev requirements updated

--- tests/test_esm_archiving.py
+++ tests/test_esm_archiving.py
@@ -3,7 +3,7 @@
 # @Email:  pgierz@awi.de
 # @Filename: test_esm_archiving.py
 # @Last modified by:   pgierz
-# @Last modified time: 2020-03-02T10:00:06+01:00
+# @Last modified time: 2020-03-09T06:56:45+01:00
 
 
 #!/usr/bin/env python
@@ -11,34 +11,53 @@
 
 """Tests for `esm_archiving` package."""
 
-
+# Standard Library
+import os
+import pickle
 import unittest
-from click.testing import CliRunner
 
+# Third-Party
+from click.testing import CliRunner
+import mock
+from pyfakefs.fake_filesystem_unittest import TestCase
+
+# This module
 from esm_archiving import esm_archiving
 from esm_archiving import cli
 
 
-class TestEsm_archiving(unittest.TestCase):
+class TestEsm_archiving(TestCase):
     """Tests for `esm_archiving` package."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        test_walk_file = os.path.join(dir_path, "exp_walk.dat")
+        with open(test_walk_file, "rb") as f:
+            rvalue = pickle.load(f)
+        self.setUpPyfakefs()
+        print("Initialized fake file system!")
+        for root, dirs, files in rvalue:
+            for dir_ in dirs:
+                print("Making directory:", os.path.join(root, dir_))
+                os.makedirs(os.path.join(root, dir_))
+            for file in files:
+                print("Making file:", os.path.join(root, file))
+                self.fs.create_file(os.path.join(root, file))
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
 
     def test_command_line_interface(self):
         """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
+        print(result.output)
         assert "esm_archiving.cli.main" in result.output
         help_result = runner.invoke(cli.main, ["--help"])
         assert help_result.exit_code == 0
+        print(help_result.output)
         assert "--help  Show this message and exit." in help_result.output
 
     def test_find_indicies(self):
@@ -111,3 +130,29 @@ class TestEsm_archiving(unittest.TestCase):
             date_format="%Y%m",
         )
         self.assertEqual(out_files, test_files)
+
+    def test_walk_through_model_dir(self):
+        """
+        Checks if walking through a modeltype file directory for a specific
+        model can clean numbers correctly.
+        """
+        grouped_files = esm_archiving._walk_through_model_dir_clean_numbers(
+            "./outdata/echam"
+        )
+        expected_answer = [
+            "./LGM_###_echam#_echam_######.grb",
+            "./LGM_###_echam#_LOG_mm_######.nc",
+            "./LGM_###_echam#_accw_######.grb",
+            "./LGM_###_echam#_co#_######.grb",
+            "./LGM_###_######.##_accw.codes",
+            "./LGM_###_echam#_BOT_mm_######.nc",
+            "./LGM_###_echam#_ATM_mm_######.nc",
+            "./LGM_###_######.##_echam.codes",
+            "./echam_output_setup_########.txt",
+            "./LGM_###_######.##_co#.codes",
+            "./LGM_###_echam#_co#.codes",
+            "./LGM_###_echam#_accw.codes",
+            "./echam_output_results_.txt",
+            "./LGM_###_echam#_echam.codes",
+        ]
+        self.assertEqual(expected_answer, grouped_files)
