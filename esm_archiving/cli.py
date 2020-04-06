f573ca7fca9152afb2c5275f57d1b9e91cc40fc3
Paul Gierz <pgierz@awi.de>
2020-04-06 12:41:30 +0200

    Moves cli documentation to be directly in the code (cool that it works that way)

--- esm_archiving/cli.py
+++ esm_archiving/cli.py
@@ -1,7 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+After installation, you have a new command in your path::
 
-"""Console script for esm_archiving."""
+    esm_archive
+
+Passing in the argument ``--help`` will show available subcommands::
+
+   Usage: esm_archive [OPTIONS] COMMAND [ARGS]...
+
+   Console script for esm_archiving.
+
+    Options:
+      --version  Show the version and exit.
+      --help     Show this message and exit.
+
+    Commands:
+      create
+      upload
+
+To use the tool, you can first ``create`` a tar archive and then use ``upload``
+to put it onto the tape server.
+
+
+Creating tarballs
+~~~~~~~~~~~~~~~~~
+
+Use ``esm_archive create`` to generate tar files from an experiment::
+
+    esm_archive create /path/to/top/of/experiment start_date end_date
+
+The arguments ``start_date`` and ``end_date`` should take the form
+``YYYY-MM-DD``. A complete example would be::
+
+    esm_archive create /work/ab0246/a270077/from_ba0989/AWICM/LGM_6hours 1850-01-01 1851-01-01
+
+The archiving tool will automatically pack up all files it finds matching these
+dates in the ``outdata`` and ``restart`` directories and generate logs in the
+top of the experiment folder. Note that the final date (1851-01-1 in this
+example) is **not included**. During packing, you get a progress bar indicating
+when the tarball is finished.
+
+Please be aware that are size limits in place on DKRZ's tape server. Any tar
+files **larger than 500 Gb will be trucated**. For more information, see:
+https://www.dkrz.de/up/systems/hpss/hpss
+
+
+Uploading tarballs
+~~~~~~~~~~~~~~~~~~
+
+A second command ``esm_archive upload`` allows you to put tarballs onto to tape server at DKRZ::
+
+    esm_archive upload /path/to/top/of/experiment start_date end_date
+
+The signature is the same as for the ``create`` subcommand. Note that for this
+to work; you need to have a properly configured ``.netrc`` file in your home
+directory::
+
+    $ cat ~/.netrc
+    machine tape.dkrz.de login a270077 password OMITTED
+
+This file needs to be readable/writable **only** for you, e.g. ``chmod 600``.
+The archiving program will then be able to automatically log into the tape
+server and upload the tarballs. Again, more information about logging onto the
+tape server without password authentication can be found here:
+https://www.dkrz.de/up/help/faq/hpss/how-can-i-use-the-hpss-tape-archive-without-typing-my-password-every-time-e-g-in-scripts-or-jobs
+"""
+
 import sys
 import os
 import pprint
