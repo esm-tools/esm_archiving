fca414e7af7e2aee926d095434a0c8c32d0d1bea
Dirk Barbi <dirk.barbi@awi.de>
2020-12-01 09:57:01 +0100

    Bump version: 4.0.0 → 5.0.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pgierz/esm_archiving",
-    version="4.0.0",
+    version="5.0.0",
     zip_safe=False,
 )
